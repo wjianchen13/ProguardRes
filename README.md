6ef6f19e25c0345dde9d94d70727d8e9d2a65351
cwj <282330332@qq.com>
2024-08-14 18:20:04 +0800

    首次提交添加代码

---
+++ README.md
@@ -0,0 +1,15 @@
+# 打包资源混淆
+
+# build.gradle转build.gradle.kts
+## 参考文档
+Kotlin build.gradle.kts配置productFlavors
+https://blog.csdn.net/HXX15501057502/article/details/137581117
+
+Android - kts文件配置应用签名
+https://blog.csdn.net/kongqwesd12/article/details/133313123?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_baidulandingword~default-0-133313123-blog-137816618.235^v43^pc_blog_bottom_relevance_base2&spm=1001.2101.3001.4242.1&utm_relevant_index=1
+
+Android Gradle 的配置从 Groovy 迁移到 KTS
+https://blog.csdn.net/qq_20613731/article/details/119766219?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_baidulandingword~default-1-119766219-blog-133313123.235^v43^pc_blog_bottom_relevance_base2&spm=1001.2101.3001.4242.2&utm_relevant_index=4
+
+AndroidStudio|本地生成APK|build.gradle.kts配置
+https://blog.csdn.net/gaopeng0071/article/details/139735827
---
+++ app/build.gradle.kts
@@ -0,0 +1,95 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.proguardres"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.proguardres"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    flavorDimensions.add("type")
+    flavorDimensions.add("version")
+
+    signingConfigs {
+        create("test1") {
+            storeFile = file("../jks/test1.jks")
+            storePassword = "123456"
+            keyAlias = "key0"
+            keyPassword = "123456"
+        }
+        create("test2") {
+            storeFile = file("../jks/test2.jks");
+            storePassword = "1234516"
+            keyAlias = "key0"
+            keyPassword = "123456"
+        }
+    }
+
+    buildTypes {
+
+        debug {
+            isDebuggable = true
+            isJniDebuggable = true
+            isZipAlignEnabled = true
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-project.txt")
+        }
+
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-project.txt")
+        }
+
+    }
+
+    productFlavors {
+        create("google") {
+            dimension = "type"
+        }
+
+        create("market") {
+            dimension = "type"
+        }
+
+        create("ayome") {
+            signingConfig = signingConfigs.getByName("test1")
+            dimension = "version"
+        }
+
+        create("ayome1") {
+            signingConfig = signingConfigs.getByName("test2")
+            dimension = "version"
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
